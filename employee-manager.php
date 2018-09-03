ab190d0bd69963be844a199183bd2710af18c9a4
Mindaugas <v.mindaugas@provenwebconcepts.nl>
2018-09-03 12:39:52 +0200

    Remember input in case of failures to allow for easier correction

--- employee-manager.php
+++ employee-manager.php
@@ -21,45 +21,53 @@
             case 'create':
                 echo"Enter text as \n'{$example}'\n";
                 $command = trim(readline("Input: "));
+                readline_add_history($command);
                 $cli->create($command);
                 break;
 
             case 'show by name':
                 echo"Enter text as \n'{$exampleName}'\n";
                 $command = trim(readline("Input: "));
+                readline_add_history($command);
                 $cli->showByName($command);
                 break;
 
             case 'show by email':
                 echo"Enter text as \n'{$exampleEmail}'\n";
                 $command = trim(readline("Input: "));
+                readline_add_history($command);
                 $cli->showByEmail($command);
                 break;
 
             case 'update':
                 echo"Enter text as \n'{$example}'\n";
                 $command = trim(readline("Input: "));
+                readline_add_history($command);
                 $cli->update($command);
                 break;
 
             case 'delete':
                 echo"Enter text as \n'{$exampleName}'\n";
                 $command = trim(readline("Input: "));
+                readline_add_history($command);
                 $cli->delete($command);
                 break;
 
             case 'delete id':
                 $command = trim(readline("Enter ID: "));
+                readline_add_history($command);
                 $cli->deleteId($command);
                 break;
 
             case 'import':
                 echo"Enter file location from disk ex:'C://file/location'\n";
                 $command = trim(readline("Input: "));
+                readline_add_history($command);
                 $cli->import($command);
                 break;
 
             case 'exit':
+                echo"Shutting down.\n";
                 break;
 
             default: print "Use '-h' for the command manual. Use 'exit' to turn the script off.";
